bd98c15f41e2fc73f4a887baf8fd567d427f4ac1
sewerynszydelko <seweryn494a@gmail.com>
2024-02-15 12:52:27 +0100

    Create base data for card creation

---
+++ scripts/card.py
@@ -0,0 +1,9 @@
+
+class Card:
+    symbols = ["♥️", "♠", "♣️", "♦️"]
+    colors = ["red", "black"]
+    values = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7,
+              "8": 8, "10": 10, "J": 10, "Q": 10, "K": 10, "A": 10}
+
+    def __init__(self) -> None:
+        pass
