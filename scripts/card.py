335a5760968102ca00ed1ee0e15ee2563c947e01
sewerynszydelko <seweryn494a@gmail.com>
2024-02-16 08:57:50 +0100

    Added representative method of card

--- scripts/card.py
+++ scripts/card.py
@@ -1,9 +1,12 @@
 
 class Card:
     symbols = ["♥️", "♠", "♣️", "♦️"]
-    colors = ["red", "black"]
     values = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7,
-              "8": 8, "10": 10, "J": 10, "Q": 10, "K": 10, "A": 10}
+              "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10, "A": 10}
 
     def __init__(self) -> None:
-        pass
+        self._symbols = self.symbols
+        self._values = self.values
+
+    def __repr__(self):
+        return f"{self._symbols} -> {self._values}"
