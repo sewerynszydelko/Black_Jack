89b49e714c0fdafc6021dc3dcbba255a3734b319
sewerynszydelko <seweryn494a@gmail.com>
2024-02-16 21:28:24 +0100

    Create deck file main class , create method'create_deck'

---
+++ scripts/deck.py
@@ -0,0 +1,13 @@
+""" Deck Class file """
+from card import Card
+import random
+
+class Deck:
+
+    def __init__(self):
+        self.cards = []
+
+    def create_deck(self):
+        for s in Card.symbols:
+            for v in Card.values:
+                self.cards.append(Card(s,v))
