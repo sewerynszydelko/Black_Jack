8e59a3348693ea742212b7ea687db9fad90bbf6d
sewerynszydelko <seweryn494a@gmail.com>
2024-02-16 21:27:39 +0100

    Reorganize init to get virables

--- scripts/card.py
+++ scripts/card.py
@@ -1,12 +1,13 @@
+""" Card file class """
 
 class Card:
     symbols = ["♥️", "♠", "♣️", "♦️"]
     values = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7,
               "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10, "A": 10}
 
-    def __init__(self) -> None:
-        self._symbols = self.symbols
-        self._values = self.values
+    def __init__(self,symbol,values) -> None:
+        self._symbols = symbol
+        self._values = values
 
     def __repr__(self):
-        return f"{self._symbols} -> {self._values}"
+        return f"{self._symbols} - {self._values}"
