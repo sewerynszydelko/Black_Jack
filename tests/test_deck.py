1159881c0774f3a2ee056424a1feb20b333c0313
sewerynszydelko <seweryn494a@gmail.com>
2024-02-17 08:35:24 +0100

    Aded pick card test if it pops from list

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -3,6 +3,7 @@ from scripts.deck import Deck
 
 test_deck = Deck()
 
+
 def test_cards():
     assert test_deck.cards == []
 
@@ -11,3 +12,9 @@ def test_create_deck():
     test_deck.create_deck()
 
     assert len(test_deck.cards) == 52
+
+
+def test_pick_card():
+    test_deck.pick_card()
+
+    assert len(test_deck.cards) == 51
