3c7e4d09d29326d62f9ab3d3af2e0d4020526f3d
sewerynszydelko <seweryn494a@gmail.com>
2024-02-18 11:43:19 +0100

    added str func , pick card and count points from card to player

--- scripts/player.py
+++ scripts/player.py
@@ -1,9 +1,20 @@
 """ Player class file """
+from deck import Deck
 from card import Card
 
-class Player(Card):
+class Player(Deck):
 
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
         self.player_cards = []
         self.player_points = 0
+
+    def __str__(self):
+        return f"Player: {self.name}\nCards: {self.player_cards}"
+
+    def pick_card(self, deck_obj):
+        self.player_cards.append(deck_obj.get_card())
+
+    def count_point(self):
+        for card in self.player_cards:
+            self.player_points += Card.values[card.get_value()]
