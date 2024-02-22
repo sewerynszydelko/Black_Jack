28cfa9bd002d9bde79ca04204e8fb728c39e5fa6
sewerynszydelko <seweryn494a@gmail.com>
2024-02-22 19:05:12 +0100

    Added docstrings

--- scripts/deck.py
+++ scripts/deck.py
@@ -4,18 +4,28 @@ import random
 
 
 class Deck:
+    """Deck of cards for Black Jack
+    """
 
     def __init__(self):
         self.cards = []
 
     def create_deck(self):
+        """create deck of cards
+        """
         self.cards.clear()
         for s in Card.symbols:
             for v in Card.values:
                 self.cards.append(Card(s, v))
 
     def get_card(self):
+        """ Get card to varible
+        Returns:
+            card(str) : pop it card from deck 
+        """
         return self.cards.pop(random.randint(0, len(self.cards)-1))
 
     def shuffling_cards(self):
+        """suffle cards in deck
+        """
         random.shuffle(self.cards)
--- scripts/player.py
+++ scripts/player.py
@@ -4,8 +4,16 @@ from card import Card
 
 
 class Player(Deck):
+    """Player class
+    Args:
+        Deck (object): Takes Deck as parent
+    """
 
-    def __init__(self, name):
+    def __init__(self, name: str):
+        """Initalize player
+        Args:
+            name (str): name/ str
+        """
         self.name = name
         self.player_cards = []
         self.player_points = 0
@@ -13,10 +21,16 @@ class Player(Deck):
     def __str__(self):
         return f"Player: {self.name}\nCards: {self.player_cards}"
 
-    def pick_card(self, deck_obj):
+    def pick_card(self, deck_obj: object):
+        """Get card for player
+        Args:
+            deck_obj (object): dec as obj
+        """
         self.player_cards.append(deck_obj.get_card())
 
     def count_point(self):
+        """Count points of cards player and clear that cards
+        """
         for card in self.player_cards:
             self.player_points += Card.values[card.get_value()]
         self.player_cards.clear()
