011864441f1fb7407efaf131f438fc87e93736e5
sewerynszydelko <seweryn494a@gmail.com>
2024-02-22 19:04:46 +0100

    Added docsting

--- scripts/card.py
+++ scripts/card.py
@@ -2,6 +2,10 @@
 
 
 class Card:
+    """ Card for game/deck
+    Returns:
+        str: symbol - value of cards
+    """
     symbols = ["♥️", "♠", "♣️", "♦️"]
     values = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7,
               "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10, "A": 10}
@@ -17,6 +21,10 @@ class Card:
 
     def __repr__(self):
         return f"{self._symbol} - {self._value}"
-    
+
     def get_value(self):
+        """ get value of accual card obj
+        Returns:
+            str: give value of card
+        """
         return self._value
