ff5a64404267652d119fdf0ee8ccc622c5438db7
sewerynszydelko <seweryn494a@gmail.com>
2024-02-24 14:43:15 +0100

    Added,fixed: method clear points of player, print points player, count_points condition

--- scripts/player.py
+++ scripts/player.py
@@ -31,6 +31,15 @@ class Player(Deck):
     def count_point(self):
         """Count points of cards player and clear that cards
         """
+        if "A" in self.player_cards[0]._value or "A" in self.player_cards[1]._value:
+            self.player_points -= 9
+
         for card in self.player_cards:
             self.player_points += Card.values[card.get_value()]
         self.player_cards.clear()
+
+    def clear_points(self):
+        self.player_points = 0
+
+    def print_points(self):
+        return print(f"{self.name} have:{self.player_points} points")
